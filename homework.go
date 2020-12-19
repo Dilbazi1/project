98da149a300d3e021ceb4a4d3dab7d4d8d3f6726
Dilbazi1 <70526693+Dilbazi1@users.noreply.github.com>
2020-12-19 23:40:24 +0500

    Update homework.go

--- homework.go
+++ homework.go
@@ -1,1 +1,11 @@
 homework
+
+
+1
+Добрый день!
+
+Подскажите, пожалуйста, в чем может причина появления данного сообщения или лучше увидеть воочию ?
+
+$ git status fatal: Not a git repository (or any of the parent directories): .git
+
+git
